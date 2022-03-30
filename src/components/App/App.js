c95c05806583bc16efa8850d606be2f883cf8974
Batoul Alkarim <batoulalkarim1@gmail.com>
2022-03-30 10:27:00 -0400

    added commits page and contact page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,8 +5,9 @@ import Login from '../Login';
 import './App.css';
 import useToken from './useToken';
 import Nav from '../Nav';
-
-
+import Contact from '../Contact';
+import Recommend from '../Recommend';
+import YourCommits from '../YourCommits';
 
 //now we should be able to login and stay logged in- if we want to test a new one
 //in the console type localStorage.clear()
@@ -31,6 +32,15 @@ function App() {
       <Switch>
         <Route path="/dashboard">
           <Dashboard />
+        </Route>
+        <Route path='/contact'>
+          <Contact />
+        </Route>
+        <Route path='/recommend'>
+          <Recommend />
+        </Route>
+        <Route path='/yourcommits'>
+          <YourCommits />
         </Route>
       </Switch>
     
---
+++ src/components/YourCommits.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function YourCommits(){
+    return(
+        <div className="commitscontainer">
+            <h1>**This is where we'll put all of the commits that have been added**</h1>
+        </div>
+    )
+}
+
+export default YourCommits 
